b4026012914076b2e788aa38b29268d9aca91d97
Vladimir Timchenko <neologfly@gmail.com>
2025-06-26 20:36:29 +0400

    fix main file

---
+++ fastapi_application/main.py
@@ -0,0 +1,35 @@
+"""main filе"""
+from contextlib import asynccontextmanager
+import uvicorn
+from fastapi import FastAPI
+
+from fastapi_application.core.config import settings
+
+
+from fastapi_application.api import router as api_router
+from fastapi_application.core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    yield
+    #shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+main_app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+)
